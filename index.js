dd4385b64f9c2fe1fa4a045bfebf5063971ab16a
Joe Longstreet <joelongstreet@gmail.com>
2014-10-08 10:48:38 -0500

    error checking to make web service run even if arduino not connected

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ var port = process.env.PORT || 3000;
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var server = connect().use(serveStatic(__dirname)).listen(port);
+console.log('server started and listening on port', port);
 
 
 // Connect to arduino
 var arduinoReady = false;
 var firmata = require('firmata');
-var board = new firmata.Board('/dev/tty.usbmodem1411', function(){
-  isReady = true;
-  board.pinMode(9,board.MODES.SERVO);
+var board = new firmata.Board('/dev/tty.usbmodem1421', function(err){
+  if(!err){
+    arduinoReady = true;
+    console.log('arduino ready');
+    board.pinMode(9,board.MODES.SERVO);
+  }
 });
 
 
-// Handle socket events
+// // Handle socket events
 var io = require('socket.io')(server);
 
 io.on('connection', function(socket){
