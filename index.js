b5acdcf0751fec584450ded0685779706f3e4656
Joe Longstreet <joelongstreet@gmail.com>
2014-12-02 10:06:45 -0600

    slightly clearer of the arduino implementation?

--- index.js
+++ index.js
@@ -13,18 +13,25 @@ console.log('server started and listening on port', port);
 io.on('connection', function(socket){
   // { pin : 5, type : 'trap-door', state : 'min', arduinoIndex : '0' }
   socket.on('update-hardware', function(params){
-    if(arduinos[params.arduinoIndex])
-      arduinos[params.arduinoIndex].updatePin(params);
+    if(arduinos[params.arduinoIndex]){
+      var arduino = findArduino(params.arduinoIndex);
+      if(arduino) arduino.updatePin(params);
+    }
   });
 
   // { pin : 5, type : 'trap-door', arduinoIndex : '0' }
   socket.on('register-hardware', function(params){
-    if(arduinos[params.arduinoIndex])
-      arduinos[params.arduinoIndex].registerPin(params);
+    if(arduinos[params.arduinoIndex]){
+      var arduino = findArduino(params.arduinoIndex);
+      if(arduino) arduino.registerPin(params);
+    }
   });
 });
 
 
+// See the readme for an explanation of the below
+// 0 - 95333353836351511280
+// 1 - 7543730383035120E070
 // find and create the arduinos
 var Arduino = require('./lib/arduino');
 var SerialPort = require('serialport');
@@ -32,7 +39,22 @@ var arduinos = [];
 
 SerialPort.list(function(err, ports){
   ports.forEach(function(port, i){
-    if(port.manufacturer.indexOf('Arduino') != -1)
-      arduinos.push(new Arduino(port, i.toString()));
+    if(port.manufacturer.indexOf('Arduino') != -1){
+      var arduino = new Arduino(port, i.toString());
+      if(port.serialNumber == '95333353836351511280')
+        arduino.index = 0;
+      else arduino.index = 1;
+
+      arduinos.push(arduino);
+    }
   });
 });
+
+var findArduino = function(index){
+  var foundArduino;
+  arduinos.forEach(function(arduino){
+    if(arduino.index == index) foundArduino = arduino;
+  });
+
+  return foundArduino;
+};
