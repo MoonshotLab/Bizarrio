ff76404c860955c9d490645fd74b5124238fa995
Joe Longstreet <joelongstreet@gmail.com>
2014-12-04 14:39:55 -0600

    fix fan button

--- public/lib/fan.js
+++ public/lib/fan.js
@@ -2,7 +2,10 @@ var Fan = function(opts){
   if(bizarrio.project || bizarrio.debug)
     opts.imagePath = 'fan';
 
-  this.type  = 'fan';
+  // this needs to change, i don't know why it's required
+  opts.y = (opts.el.y - opts.game.map.tileHeight*5);
+
+  this.type = 'fan';
   this.init(opts);
 
   return this;
@@ -13,14 +16,17 @@ Fan.prototype = Object.create(Obstacle.prototype);
 
 
 Fan.prototype.toggle = function(){
+  this.sprite.alive = this.sprite.alive ? false : true;
+
   var xGravity  = 0;
   var state     = 'lo';
+  this.sprite.alpha = 0.25;
 
-  if(this.isOn) this.isOn = false;
-  else{
+  if(this.sprite.alive){
     state     = 'hi';
     xGravity  = -5000;
     this.isOn = true;
+    this.sprite.alpha = 0.75;
   }
 
   bizarrio.game.interface.physics.arcade.gravity.x = xGravity;
